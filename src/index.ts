84ab938534431967bc4c0a7bee352dc8d0475c9c
AbdulrahmanAlosaimi <osaimi-a@hotmail.com>
2022-10-11 21:26:52 +0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+
+let app = express();
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening at port ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+app.get('/api', (req, res) => {
+  res.send('API endpoint working.');
+});
